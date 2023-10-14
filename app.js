a72669a9189dac6b32b3f875eee72dc4b6d1f788
Paavan Showri <118935216+paavan-showri@users.noreply.github.com>
2023-10-15 00:55:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/shared/FGD3XRR23?:display_count=n&:origi
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1200,
+    height: 1080,
+    width: 1920,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
